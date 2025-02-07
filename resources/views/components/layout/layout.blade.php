719255f40ce63158567bcf397112b53b8b7239b5
AldiHasan123Dev <aldiage007.if20@student.unusa.ac.id>
2025-02-07 15:21:46 +0700

    update: tokomama2/7/2025 3

--- resources/views/components/layout/layout.blade.php
+++ resources/views/components/layout/layout.blade.php
@@ -10,7 +10,7 @@
     <meta content="Themesbrand" name="author">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- App favicon -->
-    <link rel="icon" type="image/png" href="../TokomamaBahagia.PNG">
+    <link rel="icon" type="image/png" href="{{ asset('TokomamaBahagia.PNG') }}">
     <!-- Layout config Js -->
     <!-- Icons CSS -->
     {{-- @vite('resources/css/app.css') --}}
