70e339d009284815b0f12ad0bc54ffdac549cfb3
AldiHasan123Dev <aldiage007.if20@student.unusa.ac.id>
2025-02-17 14:09:37 +0700

    update: harga jual

--- resources/views/invoice/index.blade.php
+++ resources/views/invoice/index.blade.php
@@ -38,7 +38,8 @@
                                 </select>
                             </td>                       
                             <td style="border: 1px solid #ddd; padding: 12px; text-align: center;">
-                                <input onclick="this.select()" id="qty-{{ $item->id }}-1" style="border: none; text-align: center" type="number" onchange="inputBarang({{ $item->id }}, this.value,{{ $item->harga_jual }}, {{ $item->jumlah_jual }})" name="jumlah[{{ $item->id }}][]" value="{{ $item->sisa }}" disabled>
+                                <input type="hidden" id="qty-{{ $item->id }}-1" name="jumlah[{{ $item->id }}][]" value="{{ $item->sisa }}">
+                                {{ $item->sisa }}
                             </td>
                             <td style="border: 1px solid #ddd; padding: 12px; text-align: end;" id="harga_beli_ppn-{{ $item->id }}-1">
                                 @if ($item->barang->status_ppn == 'ya')    
