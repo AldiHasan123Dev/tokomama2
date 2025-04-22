816f08e17c5bf7fa27c5248d146396307293fa17
AldiHasan123Dev <aldiage007.if20@student.unusa.ac.id>
2025-04-22 13:19:08 +0700

    update: laporan ppn

--- resources/views/pajak/laporan-ppn.blade.php
+++ resources/views/pajak/laporan-ppn.blade.php
@@ -2,48 +2,47 @@
     <link rel="stylesheet" href="https://cdn.datatables.net/datetime/1.5.2/css/dataTables.dateTime.min.css">
     <style>
         #table-ppn {
-            font-size: 12px; /* Ukuran font kecil */
-            width: 100%; /* Pastikan tabel memenuhi lebar */
+            font-size: 12px;
+            width: 100%;
         }
-    
+
         #table-ppn th, #table-ppn td {
-            padding: 4px; /* Kurangi padding */
+            padding: 4px;
         }
-    
+
         #table-ppn_wrapper .dataTables_length, 
         #table-ppn_wrapper .dataTables_filter, 
         #table-ppn_wrapper .dataTables_info, 
         #table-ppn_wrapper .dataTables_paginate {
-            font-size: 10px; /* Ukuran font lebih kecil untuk kontrol tabel */
+            font-size: 10px;
         }
-    
-        #table-ppn_wrapper .dataTables_paginate .paginate_button {
-            padding: 2px 5px; /* Kurangi padding tombol paginasi */
-            font-size: 10px; /* Ukuran font tombol lebih kecil */
-        }
-        .text-center {
-    text-align: center;
-}
 
+        #table-ppn_wrapper .dataTables_paginate .paginate_button {
+            padding: 2px 5px;
+            font-size: 10px;
+        }
+
+        .text-center {
+            text-align: center;
+        }
     </style>
-    
+
     <x-pajak.card>
         <x-slot:tittle>Laporan PPN</x-slot:tittle>
+
         <div class="grid grid-cols-7 gap-4 mt-4 mb-4">
             <form action="{{ route('pajak.export.ppnexc') }}" method="post">
                 @csrf
-                <!-- Input Hidden for Start and End Date for Excel Export -->
-                <input type="hidden" name="start" id="startex" value="{{ date('Y-m-d') }}" required>
-                <input type="hidden" name="end" id="endex" value="{{ date('Y-m-d') }}" required>
-                <button type="submit" class="btn w-28 font-semibold text-white bg-green-500 hover:bg-green-400" id="excel">Export Excel</button>
+                <input type="hidden" name="start" id="startex" value="{{ date('Y-m-d') }}">
+                <input type="hidden" name="end" id="endex" value="{{ date('Y-m-d') }}">
+                <button type="submit" class="btn w-28 font-semibold text-white bg-green-500 hover:bg-green-400">Export Excel</button>
             </form>
 
             <form action="{{ route('pajak.export.ppncsv') }}" method="post">
                 @csrf
-                <!-- Input Hidden for Start and End Date for CSV Export -->
-                <input type="hidden" name="start" id="startcs" value="{{ date('Y-m-d') }}" required>
-                <input type="hidden" name="end" id="endcs" value="{{ date('Y-m-d') }}" required>
-                <button type="submit" class="btn w-28 font-semibold text-white bg-blue-500 hover:bg-blue-400" id="csv">Export CSV</button>
+                <input type="hidden" name="start" id="startcs" value="{{ date('Y-m-d') }}">
+                <input type="hidden" name="end" id="endcs" value="{{ date('Y-m-d') }}">
+                <button type="submit" class="btn w-28 font-semibold text-white bg-blue-500 hover:bg-blue-400">Export CSV</button>
             </form>
         </div>
 
@@ -51,16 +50,14 @@
 
         <div class="overflow-x-auto">
             <table border="0" cellspacing="5" cellpadding="5">
-                <tbody>
-                    <tr>
-                        <td>Tanggal Mulai:</td>
-                        <td><input type="text" id="min" name="min" class="rounded-md"></td>
-                    </tr>
-                    <tr>
-                        <td>Tanggal Selesai:</td>
-                        <td><input type="text" id="max" name="max" class="rounded-md"></td>
-                    </tr>
-                </tbody>
+                <tr>
+                    <td>Tanggal Mulai:</td>
+                    <td><input type="text" id="min" name="min" class="rounded-md"></td>
+                </tr>
+                <tr>
+                    <td>Tanggal Selesai:</td>
+                    <td><input type="text" id="max" name="max" class="rounded-md"></td>
+                </tr>
             </table>
 
             <table class="cell-border hover nowrap" id="table-ppn">
@@ -83,9 +80,37 @@
                         <th class="text-center">Total (Rp)</th>
                     </tr>
                 </thead>
-                <tbody>
-                </tbody>
+                <tbody></tbody>
             </table>
+        </div>
+
+        <div class="card shadow-sm">
+            <div class="card-header bg-primary text-white fw-bold">
+                Ringkasan Total
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered mb-0" style="min-width: 1000px;">
+                        <tbody>
+                            <tr>
+                                <th colspan="14" class="text-end align-middle">Total (Rp):</th>
+                                <td class="text-end fw-semibold" id="grand-total"></td>
+                            </tr>
+                            <tr>
+                                <th colspan="14" class="text-end align-middle">Total Sub Total (Rp):</th>
+                                <td class="text-end fw-semibold" id="grand-subtotal"></td>
+                            </tr>
+                            <tr>
+                                <th colspan="14" class="text-end align-middle">Total Nominal PPN (Rp):</th>
+                                <td class="text-end fw-semibold" id="grand-nominalPpn"></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+                   
+            
         </div>
     </x-pajak.card>
 
@@ -94,84 +119,92 @@
         <script src="https://cdn.datatables.net/datetime/1.5.2/js/dataTables.dateTime.min.js"></script>
         <script>
             $(document).ready(function() {
-                let minDate, maxDate;
+                let minDate = new DateTime('#min', { format: 'YYYY-MM-DD' });
+                let maxDate = new DateTime('#max', { format: 'YYYY-MM-DD' });
 
-                // Custom filter for date range
-                $.fn.dataTable.ext.search.push(function(settings, data, dataIndex) {
+                $.fn.dataTable.ext.search.push(function(settings, data) {
                     let min = minDate.val();
                     let max = maxDate.val();
-                    let date = new Date(data[7]); // Assuming the date is in the 8th column (index 7)
+                    let date = new Date(data[7]); // tanggal faktur
 
-                    if (
-                        (min === null && max === null) ||
+                    if ((min === null && max === null) ||
                         (min === null && date <= max) ||
                         (min <= date && max === null) ||
-                        (min <= date && date <= max)
-                    ) {
+                        (min <= date && date <= max)) {
                         return true;
                     }
                     return false;
                 });
 
-                // Initialize date pickers
-                minDate = new DateTime('#min', { format: 'YYYY-MM-DD' });
-                maxDate = new DateTime('#max', { format: 'YYYY-MM-DD' });
-
-                // Initialize DataTable
                 let table = $('#table-ppn').DataTable({
-                    pageLength: 20, // Batas data per halaman
-                    lengthMenu: [20, 50, 100, 150], // Pilihan jumlah data per halaman
-                    ordering: false,
-                    scrollX: true, 
-                    ajax: {
-                        url: "{{ route('pajak.laporan-ppn.data') }}",
-                        dataSrc: "data",
-                    },
-                    autoWidth: true,
-                    columns: [
-                        { data: 'DT_RowIndex', name: 'number', className: 'text-center' },
-                        { data: 'invoice', name: 'invoice', className: 'text-center' },
-                        { data: 'npwp', name: 'npwp', className: 'text-center' },
-                        { data: 'nik', name: 'nik', className: 'text-center' },
-                        { data: 'nama', name: 'nama', className: 'text-center' },
-                        { data: 'nama_npwp', name: 'nama_npwp', className: 'text-center' },
-                        { data: 'alamat_npwp', name: 'alamat_npwp', className: 'text-center' },
-                        { data: 'tgl_invoice', name: 'tanggal_invoice', className: 'text-center' },
-                        { data: 'tujuan', name: 'tujuan', className: 'text-center' },
-                        { data: 'uraian', name: 'uraian', className: 'text-center' },
-                        { data: 'faktur', name: 'faktur', className: 'text-center' },
-                        { data: 'subtotal', name: 'subtotal', render: $.fn.dataTable.render.number('.', ',', 2) },
-                        { data: 'ppn', name: 'ppn' },
-                        { data: 'nominal_ppn', name: 'nominal_ppn', render: $.fn.dataTable.render.number('.', ',', 2) },
-                        { data: 'total', name: 'total', render: $.fn.dataTable.render.number('.', ',', 2) },
-                        { data: 'id', name: 'id', visible: false }
-                    ]
-                });
+    pageLength: 20,
+    lengthMenu: [20, 50, 100, 150],
+    ordering: false,
+    scrollX: true,
+    scrollY: "400px",
+    scrollCollapse: true,
+    fixedHeader: {
+        footer: true
+    },
+    ajax: {
+        url: "{{ route('pajak.laporan-ppn.data') }}",
+        dataSrc: "data",
+    },
+    columns: [
+        { data: 'DT_RowIndex', className: 'text-center' },
+        { data: 'invoice', className: 'text-center' },
+        { data: 'npwp', className: 'text-center' },
+        { data: 'nik', className: 'text-center' },
+        { data: 'nama', className: 'text-center' },
+        { data: 'nama_npwp', className: 'text-center' },
+        { data: 'alamat_npwp', className: 'text-center' },
+        { data: 'tgl_invoice', className: 'text-center' },
+        { data: 'tujuan', className: 'text-center' },
+        { data: 'uraian', className: 'text-center' },
+        { data: 'faktur', className: 'text-center' },
+        { data: 'subtotal', render: $.fn.dataTable.render.number('.', ',', 2), className: 'text-right' },
+        { data: 'ppn', className: 'text-center' },
+        { data: 'nominal_ppn', render: $.fn.dataTable.render.number('.', ',', 2), className: 'text-right' },
+        { data: 'total', render: $.fn.dataTable.render.number('.', ',', 2), className: 'text-right' },
+        { data: 'id', visible: false }
+    ],
+    footerCallback: function (row, data) {
+        let api = this.api();
 
-                // Refilter the table when date inputs change
+        // Helper function
+        function getTotal(colIndex) {
+            return api.column(colIndex, { page: 'current' }).data().reduce((a, b) => {
+                let x = typeof a === 'string' ? parseFloat(a.replace(/[.]/g, '').replace(/,/g, '.')) || 0 : a;
+                let y = typeof b === 'string' ? parseFloat(b.replace(/[.]/g, '').replace(/,/g, '.')) || 0 : b;
+                return x + y;
+            }, 0);
+        }
+
+        let subtotal = getTotal(11);       // kolom subtotal
+        let nominal_ppn = getTotal(13);    // kolom nominal ppn
+        let total = getTotal(14);          // kolom total
+
+        $('#grand-subtotal').html(subtotal.toLocaleString('id-ID', { minimumFractionDigits: 2 }));
+        $('#grand-nominalPpn').html(nominal_ppn.toLocaleString('id-ID', { minimumFractionDigits: 2 }));
+        $('#grand-total').html(total.toLocaleString('id-ID', { minimumFractionDigits: 2 }));
+    }
+});
+
+
                 $('#min, #max').on('change', function() {
                     table.draw();
+                    $('#startex').val($('#min').val());
+                    $('#startcs').val($('#min').val());
+                    $('#endex').val($('#max').val());
+                    $('#endcs').val($('#max').val());
                 });
 
-                // Update export date values on change
-                $("#min").on('change', function() {
-                    var inputValue = $(this).val();
-                    $('#startex').val(inputValue); // Set value for Excel export
-                    $('#startcs').val(inputValue); // Set value for CSV export
-                });
-
-                $('#max').on('change', function() {
-                    var inputValue = $(this).val();
-                    $('#endex').val(inputValue); // Set value for Excel export
-                    $('#endcs').val(inputValue); // Set value for CSV export
-                });
                 $("form").on('submit', function(event) {
-            // Cek apakah input tanggal mulai dan selesai sudah diisi
-            if ($('#min').val() === '' || $('#max').val() === '') {
-                event.preventDefault(); // Batalkan pengiriman form
-                alert('Silakan isi tanggal mulai dan tanggal selesai terlebih dahulu.');
-            }
-        });
+                    if (!$('#min').val() || !$('#max').val()) {
+                        event.preventDefault();
+                        alert('Silakan isi tanggal mulai dan tanggal selesai terlebih dahulu.');
+                    }
+                });
             });
         </script>
     </x-slot:script>
