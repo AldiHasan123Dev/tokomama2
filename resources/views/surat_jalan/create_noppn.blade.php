46957ee94b9748c871f790cd75ab1d78f1fe8a07
AldiHasan123Dev <aldiage007.if20@student.unusa.ac.id>
2025-04-28 11:40:24 +0700

    update: fixed new kolom gudang

--- resources/views/surat_jalan/create_noppn.blade.php
+++ resources/views/surat_jalan/create_noppn.blade.php
@@ -90,7 +90,7 @@
                                                 @foreach ($barang as $item)
                                                     <option value="{{ $item->id }}">{{ $item->nama }} ||
                                                         {{ $item->nama_satuan }} || Stock : {{ $item->sisa }} ||
-                                                        {{ $item->kode_objek }} || {{ $item->no_bm }} -> {{ $item->gudang }}</option>
+                                                        {{ $item->kode_objek }} || {{ $item->no_bm }} -> {{ $item->gudang ?? 'AB' }}</option>
                                                 @endforeach
                                             </select>
                                     </td>
