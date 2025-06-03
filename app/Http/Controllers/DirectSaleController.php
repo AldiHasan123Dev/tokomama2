18018996a6e2793666100b83f7e300ac71fc26e2
AldiHasan123Dev <aldiage007.if20@student.unusa.ac.id>
2025-06-03 11:14:04 +0700

    update ds

--- app/Http/Controllers/DirectSaleController.php
+++ app/Http/Controllers/DirectSaleController.php
@@ -130,7 +130,7 @@ class DirectSaleController extends Controller
         $tgl_m = date('m', strtotime($tgl));
         $tgl_y = date('y', strtotime($tgl));
 
-        $customer = Customer::where('id', 126)->first();
+        $customer = Customer::where('id', 149)->first();
         $no = SuratJalan::whereYear('created_at', date('Y'))->max('no') + 1;
         $nama_cust = $customer->nama;
         $roman_numerals = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII"];
